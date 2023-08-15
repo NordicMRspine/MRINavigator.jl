a12c1da2e668312a04f5a550f1f797cb86d6ee9f
Laura Beghini <laura.beghini@ntnu,no>
2023-08-15 14:34:23 +0200

    added dependencies

--- src/main.jl
+++ src/main.jl
@@ -1,39 +1,14 @@
-#=
-function loadData(params::Dict{Symbol, Any})
 
-    rawData = RawAcquisitionData(ISMRMRDFile(params[:path_imgData]),
-                                slice = params[:slices],
-                                contrast = params[:echoes],
-                                repetition = params[:rep])
-    if params[:rep] != 0
-        for ii = 1:length(rawData.profiles)
-            rawData = @set rawData.profiles[ii].head.idx.repetition = 0
-        end
+
+
+
+
+
+
+
+
+
+
+    function nav_pipeline()
+
     end
-
-    OrderSlices!(rawData)
-        if params[:rep] == 0
-            noisemat = ExtractNoiseData!(rawData)
-            FileIO.save(params[:path_noise],"noisemat",noisemat)
-        else
-            noisemat = FileIO.load(params[:path_noise], "noisemat")
-        end
-        ReverseBipolar!(rawData)
-        RemoveRef!(rawData, params[:slices], params[:echoes])
-
-        (nav, nav_time) = ExtractNavigator(rawData, params[:slices])
-        nav_time = nav_time .* 2.5 # seconds from beginning of the day
-
-    rawMap = RawAcquisitionData(ISMRMRDFile(params[:path_mapData]),
-                                slice = params[:slices],
-                                contrtast = params[:echoes],
-                                repetition = params[:rep])
-    
-
-
-end
-
-
-
-COPY THE USAGE EXAMPLE HERE AND DIVIDE IT IN FUNCTIONS
-=#
