df93135e87d19e717d1171d3a4a591f7074452d3
Laura Beghini <laura.beghini@ntnu,no>
2024-04-21 22:04:10 +0200

    fix typos

--- src/SpineCenterline.jl
+++ src/SpineCenterline.jl
@@ -2,14 +2,14 @@ export ReconstructSaveMap, ReconstructMap, niftiSaveImg, callSCT, findCenterline
 
 
 """
-    ReconstructSaveMap(path_nifti::String, path_ref::String)
+    ReconstructSaveMap(path_nifti::String, path_ref::String, thresh::Float64)
 
 Reconstruct the coil sensitivity map using the MRIReco.jl function and save it in nifti format without spatial information.
 
 # Arguments
 * `path_nifti::String` - path of the nifti file. The file must have .nii extension
 * `path_rep::String` - path of reference data in ISMRMRD format
-* `tresh::Float64` - masking threshold: increase for reduced mask size, decrease for extended mask size
+* `thresh::Float64` - masking threshold: increase for reduced mask size, decrease for extended mask size
 
 MRIReco reference: https://onlinelibrary.wiley.com/doi/epdf/10.1002/mrm.28792
 ISMRMRD reference: https://onlinelibrary.wiley.com/doi/epdf/10.1002/mrm.26089
@@ -32,7 +32,7 @@ Reconstruct the coil sensitivity map using the MRIReco.jl function.
 
 # Arguments
 * `path_rep::String` - path of reference data in ISMRMRD format
-* `tresh::Float64` - masking threshold: increase for reduced mask size, decrease for extended mask size
+* `thresh::Float64` - masking threshold: increase for reduced mask size, decrease for extended mask size
 
 MRIReco reference: https://onlinelibrary.wiley.com/doi/epdf/10.1002/mrm.28792
 ISMRMRD reference: https://onlinelibrary.wiley.com/doi/epdf/10.1002/mrm.26089
